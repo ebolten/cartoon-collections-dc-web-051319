5e9d3274c183140e56a3265898433f9df84d3a80
Emily Bolten <emilybolten@gmail.com>
2019-05-07 18:41:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(names)
   names.each_with_index do |name, index|
     hashDwarfs[name] = (index + 1)
   end
-  return print hashDwarfs
+  return print "#{hashDwarfs}\n"
 end
 
 def summon_captain_planet(calls)
